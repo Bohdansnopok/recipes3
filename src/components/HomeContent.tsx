3aff632645121eed2259675772b7e8035e2ff864
Mykhail <gorgefraggggg@gmail.com>
2025-04-13 11:40:14 +0300

    update final code

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import star from "../../public/Star.svg"
 import { useQuery } from "@tanstack/react-query";
-import { UserStore, Recipe } from "@/store/UserStore";
+import { UserStore } from '@/store/UserStore'
 import starNotFilled from "../../public/starNotFilled.svg"
 import { RecipeDetails } from "./RecipeDetail";
 import { useState } from "react";
+import { Recipe } from "@/types/Store";
+import { formatDate } from "@/utils/formattedDate";
 
 export default function HomeContent() {
     const { getRecipes } = UserStore();
@@ -15,9 +17,9 @@ export default function HomeContent() {
         queryFn: getRecipes
     });
 
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
+    const [selectedRecipe, setSelectedRecipe] = useState<Recipe | null>(null);
 
-    const openModal = (recipe: any) => {
+    const openModal = (recipe: Recipe) => {
         setSelectedRecipe(recipe);
     };
 
@@ -49,13 +51,17 @@ export default function HomeContent() {
                                 />
                                 <p>{recipe?.user?.username}</p>
                             </div>
-                            <div className="flex items-center gap-[3px]">
-                                <Image src={star} alt="Star-active icon" />
-                                <Image src={star} alt="Star-active icon" />
-                                <Image src={star} alt="Star-active icon" />
-                                <Image src={star} alt="Star-active icon" />
-                                <Image src={starNotFilled} alt="Not-active star" />
-                                <div className="ml-2 text-[11px] text-[#7C7C7C]">(5)</div>
+                            <div className="flex items-center">
+                                {[...Array(5)].map((_, index) => (
+                                    <Image
+                                        key={index}
+                                        src={index < Math.round(recipe.rating) ? star : starNotFilled}
+                                        alt={index < Math.round(recipe.rating) ? "Star-active icon" : "Not-active star"}
+                                    />
+                                ))}
+                                <div className="ml-2 text-[11px] text-[#7C7C7C]">
+                                    ({recipe.rating || 0})
+                                </div>
                             </div>
                         </div>
                         <div className="px-[23px]">
@@ -63,37 +69,52 @@ export default function HomeContent() {
                             <h2 className="text-[27px] font-bold mt-4 w-[] ">{recipe?.title}</h2>
                             <p className="max-w-[513px] whitespace-nowrap overflow-hidden text-ellipsis mt-[18px]">{recipe?.caption}.</p>
                             <div className="text-[20px] text-black/66 font-medium mt-5">
-                                {new Date(recipe?.createdAt).toLocaleDateString()}
+                                {recipe?.createdAt && formatDate(recipe.createdAt)}
                             </div>
                         </div>
-                    </div>
+                    </div >
                 ))
             ) : (
                 <p>No data available</p>
-            )}
-
-
-            {selectedRecipe && (
-                <RecipeDetails>
-                    <button
-                        onClick={closeModal}
-                        className='absolute top-3 right-4 bg-transparent border-none text-[1.75rem] cursor-pointer text-[#666666]'
-                    >
-                        &times;
-                    </button>
-                    <Image src={selectedRecipe.image} alt="recipe" width={500} height={200} className="w-full" />
-                    <div className="flex items-center justify-between w-full">
-                        <div className="flex items-center">
-                            <h2 className="text-[27px] font-bold mt-4">{selectedRecipe.title}</h2>
+            )
+            }
+            {
+                selectedRecipe && (
+                    <RecipeDetails>
+                        <button
+                            onClick={closeModal}
+                            className='absolute top-3 right-4 bg-transparent border-none text-[1.75rem] cursor-pointer text-[#666666]'
+                        >
+                            &times;
+                        </button>
+                        <Image src={selectedRecipe.image} alt="recipe" width={500} height={200} className="w-full" />
+                        <div className="flex items-center justify-between w-full">
+                            <div className="flex items-center">
+                                <h2 className="text-[27px] font-bold mt-4">{selectedRecipe.title}</h2>
+                            </div>
+                            <div className="mt-4">
+                                <div className="text-[14px]">{selectedRecipe.createdAt && formatDate(selectedRecipe?.createdAt)}</div>
+                                <div className="text-yellow-700 font-bold text-[14px] text-right flex items-center justify-end">
+                                    Rating: {selectedRecipe.rating}
+                                    <div className="flex ml-2">
+                                        {[...Array(5)].map((_, index) => (
+                                            <Image
+                                                key={index}
+                                                src={index < Math.round(selectedRecipe.rating) ? star : starNotFilled}
+                                                alt={index < Math.round(selectedRecipe.rating) ? "Star-active icon" : "Not-active star"}
+                                            />
+                                        ))}
+                                        <div className="ml-2 text-[11px] text-[#7C7C7C]">
+                                            ({selectedRecipe.rating || 0})
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="mt-4">
-                            <div>{selectedRecipe.createdAt}</div>
-                            <div className="text-yellow-700 font-bold text-[20px]">rating: {selectedRecipe.rating}</div>
-                        </div>
-                    </div>
-                    <p className="mt-[18px]">{selectedRecipe.caption}</p>
-                </RecipeDetails>
-            )}
+                        <p className="mt-[18px]">{selectedRecipe.caption}</p>
+                    </RecipeDetails>
+                )
+            }
         </>
     )
 }
